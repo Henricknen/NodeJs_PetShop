5a0c0e49c68fc46b18ece7349c5b74d49991225e
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-09-06 20:37:38 -0300

    criando_'rotas'_e_'controllers'

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+
+export const home = (req: Request, res: Response) => {      // 'export' exporta a rota da página 'home' para ser possível utilizada do lado de fora
+
+    res.send('home no controller!');     // resposta da rota
+
+}
+
+export const dogs = (req: Request, res: Response) => {
+
+    // res.render('pages/page');
+
+}
+
+export const cats = (req: Request, res: Response) => {
+
+    // res.render('pages/page');
+
+}
+
+export const fishes = (req: Request, res: Response) => {
+
+    // res.render('pages/page');
+
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';     // importação do 'router'
+import * as PageController from '../controllers/pageController';        // importando 'controller' pageController
+import * as SearchController from '../controllers/searchController';
 
 const router = Router();
 
-router.get('/', (req, res)=> {      // rota 'home'
-    res.send('home');
-});
+router.get('/', PageController.home);      // rota 'home'
+router.get('/dogs', PageController.dogs);
+router.get('/cats', PageController.cats);
+router.get('/fishes', PageController.fishes);
+
+router.get('/search', SearchController.search);     // rota de 'busca'
 
 export default router;
